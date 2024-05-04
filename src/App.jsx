9faf2baa82f7a3f7b321f0f20f56d1824f65e976
modouljaiteh24 <167339136+modouljaiteh24@users.noreply.github.com>
2024-05-04 12:32:44 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <Navbar />
       <div className="bg-indigo-600 w-full h-screen">
-        <h1 className="pt-[5rem] text-yellow-500">Aspiring Developer</h1>
+        <h1 className="pt-[5rem]">Aspiring Developer</h1>
       </div>
     </div>
   );
